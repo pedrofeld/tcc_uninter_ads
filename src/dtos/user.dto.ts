efb12d77265d2b39478780b484d032b431b3ce1a
pedrofeld <pedro.gael.feld@gmail.com>
2026-09-08 19:56:48 -0300

    feat: add optional user attributers to UserDTO

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -6,4 +6,13 @@ export interface UserDTO {
     role: 'VISIONARY' | 'INVESTOR' | 'ADMIN';
     city: string;
     state: string;
+    profession: string | null;
+    studyArea: string | null;
+    biography: string | null;
+    phoneNumber: string | null;
+    linkedIn: string | null;
+    investorType: 'COMPANY' | 'INDIVIDUAL' | null;
+    companyName: string | null;
+    position: string | null;
+    companyWebsite: string | null;
 }
