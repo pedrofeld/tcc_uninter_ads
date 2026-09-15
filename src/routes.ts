5537b0127863fafb2a62d5a70d0b3d7a7b49b0f9
pedrofeld <pedro.gael.feld@gmail.com>
2026-09-15 07:34:32 -0300

    feat: require authentication in all routes (user need to be loged to execute the requests)

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,26 @@
 import {Router} from 'express';
 import {ProjectController} from './controllers/project.controller';
 import {UserController} from './controllers/user.controller';
+import { AuthController } from './controllers/auth.controller';
+import { authMiddleware } from './middlewares/auth.middleware';
 
 const routes = Router();
 const userController = new UserController();
 const projectController = new ProjectController();
+const authController = new AuthController();
 
-routes.get('/users', userController.findAll);
-routes.get('/users/:id', userController.findById);
-routes.post('/users', userController.create);
-routes.put('/users/:id', userController.update);
-routes.delete('/users/:id', userController.delete);
+routes.post('/login', authController.login);
 
-routes.get('/projects', projectController.findAll);
-routes.get('/projects/:id', projectController.findById);
-routes.post('/projects', projectController.create);
-routes.put('/projects/:id', projectController.update);
-routes.delete('/projects/:id', projectController.delete);
+routes.get('/users', authMiddleware, userController.findAll);
+routes.get('/users/:id', authMiddleware, userController.findById);
+routes.post('/users', authMiddleware, userController.create);
+routes.put('/users/:id',  authMiddleware, userController.update);
+routes.delete('/users/:id', authMiddleware, userController.delete);
+
+routes.get('/projects', authMiddleware, projectController.findAll);
+routes.get('/projects/:id', authMiddleware, projectController.findById);
+routes.post('/projects', authMiddleware, projectController.create);
+routes.put('/projects/:id', authMiddleware, projectController.update);
+routes.delete('/projects/:id', authMiddleware, projectController.delete);
 
 export {routes};
