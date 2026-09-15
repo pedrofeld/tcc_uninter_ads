3cb4640da3c615cb1f7ddcef2dfb4d9a37ec58d9
pedrofeld <pedro.gael.feld@gmail.com>
2026-09-15 07:32:38 -0300

    feat: create authMiddleware to authenticate user login

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,59 @@
+import type { Request, Response, NextFunction } from 'express';
+import { UserRepository } from '../database/user.repository';
+import { handleError } from '../config/error.handler';
+import { JwtService } from '../services/jwt.service';
+
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const authorizationHeader = req.headers.authorization;
+
+        if (!authorizationHeader) {
+            return res.status(401).json({
+            ok: false,
+            message: 'Authentication token is required'
+            });
+        }
+
+        const token = authorizationHeader.toLowerCase().startsWith('bearer ')
+            ? authorizationHeader.slice(7).trim()
+            : authorizationHeader.trim();
+
+        if (!token) {
+            return res.status(401).json({
+            ok: false,
+            message: 'Authentication token is required'
+            });
+        }
+
+        const payload = new JwtService().validateToken(token);
+
+        if (!payload) {
+            return res.status(401).json({
+            ok: false,
+            message: 'Invalid or expired token'
+            });
+        }
+
+        const userId = payload.id;
+        const userRepository = new UserRepository();
+        const user = await userRepository.findById(userId);
+
+        if (!user) {
+            return res.status(401).json({
+            ok: false,
+            message: 'User not found'
+            });
+        }
+
+        (req as any).user = {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+        };
+
+        next();
+    } catch (error: any) {
+        return handleError(error);
+    }
+};
