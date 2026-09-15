95cc912c3e40f99ef2a2928c17f83008ec3bd072
pedrofeld <pedro.gael.feld@gmail.com>
2026-09-15 07:26:34 -0300

    feat: create AuthController

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,30 @@
+import type { Request, Response } from "express";
+import { AuthService } from "../services/auth.service";
+
+const authService = new AuthService();
+
+export class AuthController {
+  async login(req: Request, res: Response) {
+    try {
+        const { email, password } = req.body;
+        const result = await authService.login(email, password);
+        if (!result) {
+            return res.status(401).json({
+                ok: false,
+                message: "Invalid login credentials",
+            });
+        }
+        res.status(200).json({
+            ok: true,
+            message: "Login successful",
+            data: result,
+        });
+    } catch (error: any) {
+        res.status(401).json({
+            ok: false,
+            message: "Login failed",
+            error: error.message,
+        });
+    }
+  }
+}
